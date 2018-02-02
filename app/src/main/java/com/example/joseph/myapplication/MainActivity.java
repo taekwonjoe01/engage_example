1f0b6685708393240194ffdf331a751f0b35da3f
Joseph Hutchins <joeyh89@vt.edu>
2018-02-02 09:13:59 -0800

    Create master-detail activities.

--- app/src/main/java/com/example/joseph/myapplication/MainActivity.java
+++ app/src/main/java/com/example/joseph/myapplication/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.joseph.myapplication;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,31 +35,66 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    protected void onStart() {
+        super.onStart();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.hostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+        OkHttpClient client = builder.build();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://www.alphavantage.co/query").newBuilder();
+        urlBuilder.addQueryParameter("function", "TIME_SERIES_DAILY");
+        urlBuilder.addQueryParameter("symbol", "AMZ");
+        urlBuilder.addQueryParameter("datatype", "json");
+        urlBuilder.addQueryParameter("apikey", "GJLF3CX2MZ6ATP09");
+        String url = urlBuilder.build().toString();
+        Log.e("Joey", "url: " + url);
 
-        return super.onOptionsItemSelected(item);
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    Log.e("Joey", "Failure! " + response.message());
+                } else {
+                    Log.e("Joey", "Success! " + response.message());
+
+                    String jsonData = response.body().string();
+                    Log.e("Joey", jsonData);
+                    JSONObject Jobject = null;
+                    try {
+                        Jobject = new JSONObject(jsonData);
+                        Log.e("Joey", Jobject.toString());
+                        //JSONArray Jarray = Jobject.getJSONArray("employees");
+
+                        /*for (int i = 0; i < Jarray.length(); i++) {
+                            JSONObject object     = Jarray.getJSONObject(i);
+                        }*/
+                        // do something wih the result
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 }
